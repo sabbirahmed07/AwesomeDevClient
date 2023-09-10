91c60ecdfa71205149293403609b6c3004d90f03
sabbirahmed07 <sabbirsristy@gmail.com>
2023-09-10 15:25:18 +0600

    update proxy and baseUrl

--- src/redux/utils/setAuthToken.js
+++ src/redux/utils/setAuthToken.js
@@ -1,12 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-const setAuthToken = token => {
-  if (token) {
+const setAuthToken = (token) => {
+  axios.defaults.baseURL = 'https://awesomedevs-api.onrender.com';
+  if (!!token) {
     //apply to every req
-    axios.defaults.headers.common["Authorization"] = token;
+    axios.defaults.headers.common['Authorization'] = token;
   } else {
     //delete auth header
-    delete axios.defaults.headers.common["Authorization"];
+    delete axios.defaults.headers.common['Authorization'];
   }
 };
 

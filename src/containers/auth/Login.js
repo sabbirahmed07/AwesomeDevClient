262b0c25e149bec05a1bef1827d2e4a2fa1452ce
Sabbir Ahmed Sristy <sabbirsristy@gmail.com>
2025-06-18 17:06:43 +0600

    Update Login.js

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -59,7 +59,7 @@ class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">
-                Sign in to your Developer account on Bishu Digital
+                Sign in to your Developer account on Awesome Devs
               </p>
               <form onSubmit={this.onSubmit}>
                 <TextFieldGroup
